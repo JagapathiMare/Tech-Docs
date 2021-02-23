9d1e19cb5abd50fbbb1f3f6a5433f2dabbf288a2
Jagapathi <54583266+JagapathiMare@users.noreply.github.com>
2021-02-23 16:46:07 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'Git & Github documentation'
+copyright = '2021, Jagapathi'
+author = 'Jagapathi'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
